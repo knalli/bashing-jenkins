95fbee5bd760195582e35de314ebee845686b49d
Jan Philipp <knallisworld@fgooglemail.com>
2011-08-17 17:05:33 +0200

    renamed commands being no non-camel-case
    added get-build-state

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -5,10 +5,11 @@
 # created by Jan Philipp <knallisworld@googlemail.com>
 # hosted on github @ https://github.com/knalli/bashing-jenkins
 
-export BASHING_JENKINS_VERSION="0.0.1"
+export BASHING_JENKINS_VERSION="0.0.2"
 export LOCAL_VERBOSE="yes"
 # Changelog
-# 2011-02-17 Initial Release 0.0.1
+# 2011-08-17 State				0.0.2
+# 2011-02-17 Initial Release 	0.0.1
 
 
 # FUNCTIONS
@@ -28,17 +29,25 @@ function printHelp(){
 	echo ""
 	echo "Standard API: Methods"
 	echo ""
-	echo "	getBuildNumberFor <types>"
+	echo "	get-build-number <types>"
 	echo "		types: (lastBuild|lastStableBuild|lastSuccessfulBuild|lastFailedBuild|lastUnsuccessfulBuild|lastPromotedBuild <level>)"
 	echo ""
 	echo "		Will return the specified build number. The type lastPromotedBuild requires an additional argument level."
 	echo ""
 	echo "		Examples:"
-	echo "			$0 test-project getBuildNumberFor lastBuild"
-	echo "			$0 test-project getBuildNumberFor lastPromotedBuild 2"
+	echo "			$0 test-project get-build-number lastBuild"
+	echo "			$0 test-project get-build-number lastPromotedBuild 2"
 	echo ""
 	echo ""
-	echo "	downloadArtifact <buildNumber> <relativeName> [<saveAs>]"
+	echo "	get-build-state"
+	echo "		Will return the specified build state."
+	echo ""
+	echo "		Examples:"
+	echo "			$0 test-project get-build-state"
+	echo "			$0 test-project get-build-state"
+	echo ""
+	echo ""
+	echo "	download-artifact <buildNumber> <relativeName> [<saveAs>]"
 	echo "		buildNumber: a valid build number (digits only)"
 	echo "		relativeName: in general a relative path pointing to an artifact"
 	echo "		saveAs: is optional. Unless saveAs is specified the download will saved in the current working directory."
@@ -69,7 +78,7 @@ function assertServerIsAvailable(){
 
 function fetchPage(){
 	# 1 url
-	echo `lynx -dump $1`
+	echo `curl "$1" 2>/dev/null`
 	return 0
 }
 
@@ -121,6 +130,13 @@ function downloadJobArtifact(){
 	return 0
 }
 
+# Return the latest build state for the specified job.
+function getJobBuildState(){
+	# 1 job name, 2 type
+	echo $(fetchPage "$JENKINS_SERVER_URL/job/$1/$2/api/xml?xpath=/mavenModuleSetBuild/result/text()")
+	return 0
+}
+
 # MAIN
 
 # Load special settings (e.g. no enviroments set up)
@@ -156,7 +172,7 @@ if [[ $? != 0 ]]
 	exit 1
 fi
 
-if [[ "$1" = "getBuildNumberFor" ]]
+if [[ "$1" = "get-build-number" ]]
 	then
 	if [[ "$3" =~ (lastBuild|lastStableBuild|lastSuccessfulBuild|lastFailedBuild|lastUnsuccessfulBuild) ]]
 		then
@@ -175,7 +191,22 @@ if [[ "$1" = "getBuildNumberFor" ]]
 	fi
 fi
 
-if [[ "$1" = "downloadArtifact" ]]
+if [[ "$1" = "get-build-state" ]]
+	then
+	if [[ "$3" =~ (lastBuild|lastStableBuild|lastSuccessfulBuild|lastFailedBuild|lastUnsuccessfulBuild) ]]
+		then
+		echo $(result $(getJobBuildState "$2" "$3"))
+		exit 0
+	else
+		if [[ $LOCAL_SHUTUP != 'yes' ]] 
+			then
+			echo "Type $3 is not supported."
+		fi
+		exit 1
+	fi
+fi
+
+if [[ "$1" = "download-artifact" ]]
 	then
 	echo $(result $(downloadJobArtifact "$2" "$3" "$4" "$5"))
 fi
